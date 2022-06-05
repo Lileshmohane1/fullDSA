5981f8c24c0d648206b0d74ac2ad59054eb463e3
Lileshmohane1 <98868493+Lileshmohane1@users.noreply.github.com>
2022-06-05 08:30:22 +0530

    Add files via upload

---
+++ 1_conditional_statement.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+//if statement 
+/* int a,b; //take a input
+cin>>a>>b;
+if (a>b)
+{
+cout<<"a is geater\n";
+}
+else if (a<b)
+{
+cout<<"b is greater than to a \n";
+}
+else
+{
+
+    cout<<" a is not geater";
+}
+*/
+/*int a =2 ;
+int b= a+1; //3
+if ((a=3)==b){
+    cout<<a;
+}
+else
+{
+ cout<<a+1;
+}
+*/
+ /*int n;
+cin>>n;
+int i = 1;
+while ( i<=n)
+{
+    cout<<i<<endl;
+    i++;
+
+}*/
+/*int n ;
+cin>>n;
+int i =1;
+ int sum =0;
+while ( i<=n)
+{
+    sum = sum +1 ;
+}
+
+cout<<" your  valu of  sum  is "<<sum<<endl;
+
+*/
+// chake a prime number or not 
+/*int n ;
+cin>>n;
+int i =2 ;
+while (i < n)
+{
+  if (n%i==0)
+  {
+      cout<<"this number  is not a prime number "<<i<<endl;
+
+  }else
+  {
+      cout <<"this number is a prime"<<i;
+  }
+}
+
+*/
+/*
+int n;
+cin>>n;
+int i=1;
+
+while (i<=n)
+{
+    int j=1;
+   while (j<=n)
+{ 
+     cout<<"*";
+    j++;
+} 
+cout<<"\n";
+i++;
+}
+*/
+int n;
+cin>>n;
+int i=1;
+while (i<=n)
+{
+    int j=1;
+   while (j<=n)
+{ 
+     cout<<""<<i;
+    j++;
+} 
+cout<<"\n";
+i++;
+}
+
+
+return 0;
+}
